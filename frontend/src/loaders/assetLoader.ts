a72efad7b71a2ead751d4d5395dd2904f887d5b3
akmatoff <azim.akmatov1@gmail.com>
2026-01-03 03:57:39 +0600

    fix asset loading

--- frontend/src/loaders/assetLoader.ts
+++ frontend/src/loaders/assetLoader.ts
@@ -1,31 +1,37 @@
 import { Assets } from "pixi.js";
 
+import bgLandscape from "../assets/sprites/bg-landscape.png";
+import bench from "../assets/sprites/the-bench.png";
+import streetLamp from "../assets/sprites/street-lamp.png";
+import streetLampBulb from "../assets/sprites/street-lamp-bulb.png";
+import player from "../assets/sprites/player-spritesheet.png";
+
 export const preloadAssets = async () => {
   Assets.init();
 
   Assets.add({
     alias: "background",
-    src: "src/assets/sprites/bg-landscape.png",
+    src: bgLandscape,
   });
 
   Assets.add({
     alias: "bench",
-    src: "src/assets/sprites/the-bench.png",
+    src: bench,
   });
 
   Assets.add({
     alias: "streetLamp",
-    src: "src/assets/sprites/street-lamp.png",
+    src: streetLamp,
   });
 
   Assets.add({
     alias: "streetLampBulb",
-    src: "src/assets/sprites/street-lamp-bulb.png",
+    src: streetLampBulb,
   });
 
   Assets.add({
     alias: "player",
-    src: "src/assets/sprites/player-spritesheet.png",
+    src: player,
   });
 
   await Assets.load([
